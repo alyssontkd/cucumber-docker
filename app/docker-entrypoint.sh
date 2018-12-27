35b5387472d2a1be0bd226c82e2034825beb6d5e
Alysson Vicuña <root@tetsrvdocker03.poupex.com.br>
2018-12-27 17:53:23 -0200

    [UPDATE] Ajustando a estrutura para criar ambeinte de forma automatica

---
+++ app/docker-entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+git_user='71225919134'
+git_pswd='#Magatti2018#'
+glpi_dir='/var/www/html/dev.glpi.com.br'
+glpi_cred_file="${glpi_dir}/.git-credentials"
+script_banco_glpi='/var/www/html/script_inicial_glpi_docker.sql'
+
+echo "[ ****************** ] Starting Endpoint of Application"
+if ! [ -d "/var/www/html/dev.glpi.com.br/vendor" ]; then
+    echo "Application not found in /var/www/html/dev.glpi.com.br - Downloading now..."
+    if [ "$(ls -A)" ]; then
+        echo "WARNING: /var/www/html/dev.glpi.com.br is not empty - press Ctrl+C now if this is an error!"
+        ( set -x; ls -A; sleep 10 )
+    fi
+    echo "[ ****************** ] Execute the clone of the GLPI POUPEX"
+    
+    cd /var/www/html
+    mkdir -p ${glpi_dir}
+    cd ${glpi_dir}
+    git init
+    git remote add origin https://gitlab.poupex.com.br/poupex/software-livre/glpi-poupex.git
+    git config credential.helper "store --file=${glpi_cred_file}"
+    echo "https://${git_user}:${git_pswd}@gitlab.poupex.com.br" > "${glpi_cred_file}"
+    git remote update
+    git checkout -f master
+
+    ls -la ${glpi_dir}
+
+fi
+
+echo "[ ****************** ] Changing owner and group from the Project to 'www-data' "
+chmod 775 /var/www/html -Rf
+chown www-data:www-data -R ${glpi_dir}
+
+echo "[ ****************** ] Importing data in database before ending buinid of Application"
+#Realiza a carga da base de dados
+mysql -u root -h database-mysql-glpi -p12345678 < ${script_banco_glpi}
+
+echo "[ ****************** ] Ending Endpoint of Application"
+cd ${glpi_dir}
+mv install install.old
+echo "[ ****************** ] Successfull!!!!!!!!"
+exec "$@"
+
